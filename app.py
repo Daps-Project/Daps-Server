c011a9d0bff94826334b148d91489d4fd0ee5bf7
jamaliethenor <jamalie10>
2021-05-08 23:37:13 -0400

    /data path returns json object

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
+import xmltodict
 from flask import Flask
 from flask import jsonify
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/data', methods=['GET', 'POST'])
+def jsonify_XML():
+    with open("Questions.xml") as xml_file:
+        data_dict = xmltodict.parse(xml_file.read())
+    return jsonify(data_dict)
 
 if __name__ == '__main__':
     app.run()
