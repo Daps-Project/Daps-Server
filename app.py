babf1c1de3bd7805f3353ba54fff0cfc8edaa4d9
jamaliethenor <jamalie10>
2021-05-26 00:53:17 -0400

    Server receives responses in json form

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import xmltodict
-from flask import Flask
+from flask import Flask, request
 from flask import jsonify
 
 app = Flask(__name__)
@@ -13,6 +13,14 @@ def jsonify_XML():
     with open("Questions.xml") as xml_file:
         data_dict = xmltodict.parse(xml_file.read())
     return jsonify(data_dict)
+
+
+@app.route("/survey_data", methods=["GET", "POST"])
+def surveyComplete():
+    responses = request.json
+    print(responses)
+    return responses
+
 
 if __name__ == '__main__':
     app.run()
