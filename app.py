66882623b5626257806da61cd37baae389365a1c
jamaliethenor <jamalie10>
2021-05-30 01:01:55 -0400

    Server makes a Google Places API request. The place_id is captured and cached.

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ import xmltodict
 from flask import Flask, request
 from flask import jsonify
 import requests, json
+from flask_caching import Cache
+
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300}
 
 app = Flask(__name__)
+app.config.from_mapping(config)
+cache = Cache(app)
+
 
 @app.route('/')
 def hello_world():
@@ -16,31 +25,37 @@ def jsonify_XML():
     return jsonify(data_dict)
 
 
+
 @app.route("/survey_data", methods=["GET", "POST"])
-def surveyComplete():
-    responses = request.json
-    query = (responses['responses'])
-    googlePlaces(query)
+@cache.cached(timeout=120)
+def googlePlaces():
+    if request.method == 'POST':
+        responses = request.json
+        query = (responses['responses'])
+        #         // https://developers.google.com/maps/documentation/places/android-sdk/start
+        api_key = 'AIzaSyDrzDcMBvxz0DSFIhm0vrzRDAaZi1VOOjs'
+        query = "%20".join(query)
+        # url variable store url
+        url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
+        apiRequest = requests.get(url + 'input=' + query + '&inputtype=textquery&fields=place_id' +
+                         '&key=' + api_key)
+        requestJSON = apiRequest.json()
 
+        resultDicts = requestJSON['results']
+        place_id = dict()
 
-def googlePlaces(query):
-    #         // https://developers.google.com/maps/documentation/places/android-sdk/start
-    api_key = 'AIzaSyDrzDcMBvxz0DSFIhm0vrzRDAaZi1VOOjs'
-    query = "%20".join(query)
-    # url variable store url
-    url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
-    request = requests.get(url + 'input=' + query + '&inputtype=textquery&fields=place_id' +
-                     '&key=' + api_key)
-    requestJSON = request.json()
+        if resultDicts:
+            for i in range(len(resultDicts)):
+                place_id = (resultDicts[i]['place_id'])
+            print(resultDicts)
+            print(place_id)
 
-    resultDicts = requestJSON['results']
+            return (place_id)
+        else:
+            return "No results"
 
-    for i in range(len(resultDicts)):
-        print(requestJSON)
-        print(resultDicts[i]['name'])
-
-    return requestJSON
-
+    if request.method == 'GET':
+        return cache
 
 
 if __name__ == '__main__':
